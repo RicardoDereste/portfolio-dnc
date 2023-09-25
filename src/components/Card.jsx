5304cd9ca1c3011f7b2646446f05ce00ce6082b9
Ricardo Dereste Junior <129495995+RicardoDereste@users.noreply.github.com>
2023-09-25 13:03:50 +0100

    Add files via upload

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import styles from './Card.module.css'
 import ButtonB from './ButtonB'
+import { useState } from 'react'
 
-const Card = ({img, title, tech, description, repo, website}) => {
+const Card = ({ img, title, tech, description, repo, website }) => {
+
+  const [info, setInfo] = useState(false)
+
+  function infoOn() {
+    setInfo(true)
+  }
+
+  function infoOff() {
+    setInfo(false)
+  }
+
   return (
-    <div className={styles.card}>
-        <a href={website}>
-            <img src={img} alt={title} />
-        </a>
+    <div onMouseLeave={infoOff} className={styles.card}>
+      <a onMouseEnter={infoOn} href={website}>
+        <img src={img} alt={title} />
+      </a>
+
+      {info === true && (
         <section>
-            <h3>{title}</h3>
-            <p><strong>Technology: </strong>{tech}</p>
-            <p>{description}</p>
-            <ButtonB text='Access my repository' />
+          <h3>{title}</h3>
+          <p><strong>Technology: </strong>{tech}</p>
+          <p>{description}</p>
+          <ButtonB text='Access my repository' link={repo} />
         </section>
+      )}
+
     </div>
   )
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
-import styles from './Resume.module.css'
-import ButtonA from './ButtonA'
+import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import styles from './Resume.module.css';
+import ButtonA from './ButtonA';
 
 const Resume = () => {
+  const [text, setText] = useState('');
+  const [loop, setLoop] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const period = 100;
+  const [delta, setDelta] = useState(100);
+
+  const toRotate = useMemo(() => ['Ricardo Dereste!', 'a Frontend Developer!'], []);
+
+  const toType = useCallback(() => {
+    let i = loop % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setDelta(period);
+      setLoop(loop + 1);
+    }
+  }, [loop, isDeleting, text, toRotate]);
+
+  useEffect(() => {
+    let clock = setInterval(() => {
+      toType();
+    }, delta);
+
+    return () => {
+      clearInterval(clock);
+    };
+  }, [toType, delta]);
+
   return (
     <div className={styles.resume} id='resume'>
-      <h4><strong>Welcome to my Portfolio</strong></h4>
-      <h1>Hello, I'm Ricardo Dereste!</h1>
-      <p>I'm Ricardo, a 24 years year old web developer dedicated to creating engaging websites. Skilled in HTML, CSS, JavaScript, Sass, and React. Committed to continuous learning and delivering exceptional web experiences. Eager to contribute and collaborate.</p>
+      <h4>
+        <strong>Welcome to my Portfolio</strong>
+      </h4>
+      <h1>Hello, I'm {text}</h1>
+      <p>
+        I'm Ricardo, a 24 years old web developer dedicated to creating engaging websites. Skilled in HTML, CSS, JavaScript, Sass, and React. Committed to continuous learning and delivering exceptional web experiences. Eager to contribute and collaborate.
+      </p>
       <ButtonA text='Connect with me!!' link='https://github.com/RicardoDereste' />
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
